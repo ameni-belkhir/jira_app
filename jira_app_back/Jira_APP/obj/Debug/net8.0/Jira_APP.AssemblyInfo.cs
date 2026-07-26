5aecd672c4e532847067f3334d326791d3cd9aa8
Ameni <Ameni.Belkhir@esprit.tn>
2026-07-26 11:54:11 +0100

    chore(clean): remove empty Services folder include from Jira_APP.csproj

--- jira_app_back/Jira_APP/obj/Debug/net8.0/Jira_APP.AssemblyInfo.cs
+++ jira_app_back/Jira_APP/obj/Debug/net8.0/Jira_APP.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jira_APP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4162006151895220aa15f397e61f82bacd62f47b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465843d9b048d007fbc3e7e88a4b5e5210c0e31e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jira_APP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jira_APP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
