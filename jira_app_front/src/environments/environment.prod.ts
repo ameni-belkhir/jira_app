473f180c7e5b8b253382f6016e79ca3714e1df6e
Ameni <Ameni.Belkhir@esprit.tn>
2026-08-17 17:57:06 +0100

    feat(frontend): add Dockerfile multi-stage, nginx config, and environment.prod for local Docker testing

---
+++ jira_app_front/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://jira-app-backend.onrender.com/api',
+  baseUrl: 'https://jira-app-backend.onrender.com',
+  signalrHubUrl: 'https://jira-app-backend.onrender.com/hubs/notifications',
+};
